d011058cdef55031af8791628762af23305ad268
roncan <65841016+roncan1@users.noreply.github.com>
2021-03-28 23:02:01 +0900

    Initial commit

---
+++ app/src/main/java/com/CannonRandomNumber/main/CannonActivity.java
@@ -0,0 +1,78 @@
+package com.CannonRandomNumber.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class CannonActivity extends AppCompatActivity {
+
+    TextView textView_result;
+    Button button_shot;
+    ImageView green_ball;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cannon);
+
+        textView_result = findViewById(R.id.TextView_result);
+        button_shot = findViewById(R.id.Button_shot);
+        green_ball = findViewById(R.id.greenball);
+
+        final Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();//값 넘겨받기
+
+        final int N = bundle.getInt("N");
+        final boolean spanking = bundle.getBoolean("spanking");
+        final Random random = new Random();
+        final int array[] = new int[N];
+        final int[] A = {0};
+        final Animation shot1 = AnimationUtils.loadAnimation(this,R.anim.ball_start);
+
+
+
+        for (int i = 0; i <= N-1; i++) {
+            array[i] = random.nextInt(N) + 1;
+            for (int j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+                    i--;
+                }
+            }
+        }
+
+        button_shot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                green_ball.startAnimation(shot1);
+                textView_result.setText("");
+
+                new Handler().postDelayed(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {                              //딜레이 넣을 공간
+                        if (spanking == false) { //중복체거 체크가 해제되있을 경우
+                            int result = random.nextInt(N);
+                            textView_result.setText("" + result);
+                        } else { //중복제거 체크가 설정되있을 경우
+                            textView_result.setText("" + array[A[0]]);
+                            A[0]++;
+                        }
+                    }
+                }, 1500); // 딜레이 시간
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/CannonRandomNumber/main/MainActivity.java
@@ -0,0 +1,44 @@
+package com.CannonRandomNumber.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText_N;
+    Button button_next;
+    CheckBox checkBox_spanking;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText_N = findViewById(R.id.EditText_N);
+        button_next = findViewById(R.id.Button_next);
+        checkBox_spanking = findViewById(R.id.Checkbox_spanking);
+
+        button_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int N = Integer.parseInt(editText_N.getText().toString());
+
+                Intent intent = new Intent(MainActivity.this, CannonActivity.class);
+                intent.putExtra("N", N);//숫자값 보내주기
+
+                if (checkBox_spanking.isChecked() == true) { //중복제거 값을 보내주기
+                    intent.putExtra("spanking", true);
+                } else {
+                    intent.putExtra("spanking", false);
+                }
+
+                startActivity(intent);//화면전환
+            }
+        });
+    }
+}
